37f932538af30bb3941edbebc75b9e9c20a31fb9
yuheejoon <yuhetube4@gmail.com>
2023-05-15 14:18:13 +0900

    사진추가1

---
+++ src/reducers/initialState.js
@@ -0,0 +1,91 @@
+export const initialState =
+{
+  "merchandise": [
+    {
+      "id": 1,
+      "name": "애플워치 스트랩",
+      "img": "../merchandiseimag/1.png",
+      "special" : 15,
+      "price": 12000
+    },
+    {
+      "id": 2,
+      "name": "스킨 케어 세트",
+      "img": "../merchandiseimag/2.png",
+      "special" : 7,
+      "price": 39900
+    },
+    {
+        "id": 3,
+        "name": "무선 이어폰",
+        "img": "../merchandiseimag/3.png",
+        "special" : 33,
+        "price": 24900
+      },
+      {
+        "id": 4,
+        "name": "운동화",
+        "img": "../merchandiseimag/4.png",
+        "special" : 0,
+        "price": 199000
+      },
+      {
+        "id": 5,
+        "name": "샌들",
+        "img": "../merchandiseimag/5.png",
+        "special" : 12,
+        "price": 26900
+      },
+      {
+        "id": 6,
+        "name": "향수 125ml",
+        "img": "../merchandiseimag/6.png",
+        "special" : 0,
+        "price": 129000
+      },
+      {
+        "id": 7,
+        "name": "도리토스 와사비맛",
+        "img": "../merchandiseimag/7.png",
+        "special" : 70,
+        "price": 900
+      },
+      {
+        "id": 8,
+        "name": "수박 스무디",
+        "img": "../merchandiseimag/8.png",
+        "special" : 15,
+        "price": 1500
+      },
+      {
+        "id": 9,
+        "name": "팬케이크 만들기",
+        "img": "../merchandiseimag/9.png",
+        "special" : 12,
+        "price": 4500
+      },
+      {
+        "id": 10,
+        "name": "닭가슴살 미트볼",
+        "img": "../merchandiseimag/10.png",
+        "special" : 20,
+        "price": 2500
+      },
+      {
+        "id": 11,
+        "name": "과제 골다담기",
+        "img": "../merchandiseimag/11.png",
+        "special" : 10,
+        "price": 1200
+      },
+      {
+        "id": 12,
+        "name": "인테이러용 조화",
+        "img": "../merchandiseimag/12.png",
+        "special" : 0,
+        "price": 990
+      },
+  ],
+  "cartItems": [
+  ]
+}
