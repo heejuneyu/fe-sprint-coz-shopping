7f6f1a5a70168e9d3ee59e371829fff3248029ac
heejuneyu <104719146+heejuneyu@users.noreply.github.com>
2023-05-15 12:39:27 +0900

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+        
+const ModalContent = styled.div`
+    width: 160px;
+    height: 110px;
+    box-shadow: 0px 0px 6px 0px #00000040;
+    background-color: #FFFF;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    margin-top: 70px;
+    z-index: 3;
+    margin-left:82%;
+
+    &::before {
+    content: "";
+    position: absolute;
+    top: -10px;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-left: 10px solid #fff;
+    transform: translateX(-50%);
+    z-index: 3;
+  }
+`    
+
+const Header = () => {
+        const [isOpen, setIsOpen] = useState(false);
+      
+        const openModalHandler = () => {
+          // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+          setIsOpen(!isOpen)
+        };
+    return (
+         <div class="headerbox">
+        <img id = "image1" src= "logo1.png" ></img>
+        <div class="sitename">
+        <Link to="/" style={{ textDecoration: "none" }}>COZ Shopping</Link>
+        </div>
+        <img class="HamburgerBtn" src = "Hamburgericon.png" onClick={openModalHandler}></img>
+        {isOpen ? (
+          <ModalContent>
+          
+            <div id ="menu">
+              <p id= "modallist">000님, 안녕하세요!</p>
+              <Link to="/itemlistpage" style={{ textDecoration: "none" }}>상품리스트 페이지</Link>
+              <Link to="/bookmarkpage" style={{ textDecoration: "none" }}>북마크 페이지</Link>
+              </div> 
+          </ModalContent>
+        ): null}
+        </div>
+    );
+};
+  
+export default Header;
---
+++ src/pages/mainpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Mainpage(){
+    return(
+        <div id="item-list-container">
+      <div id="item-list-body">
+        <div id="item-list-title">리스트 내역</div>
+      </div>
+    </div>
+    )
+}
